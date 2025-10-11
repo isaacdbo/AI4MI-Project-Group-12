a84511a9a5bcca6607ca96f9a938b579f3b8f771
Shivam Badal <55099336+sribadal@users.noreply.github.com>
2025-10-11 16:32:54 +0200

    added best performant loss function (#4)
    * add dice and CeAndDice loss functions
    
    * add several other loss functions
    
    * remove non performant loss functions
    
    * add weighted ce and dice combined loss function
    
    * remove unneeded loss functions from main script

--- losses.py
+++ losses.py
@@ -46,8 +46,191 @@ class CrossEntropy():
         loss /= mask.sum() + 1e-10
 
         return loss
+    
+class WeightedCrossEntropy:
+    def __init__(self, **kwargs):
+        import torch
+        self.idk = kwargs['idk']
+        weights = kwargs.get('weights', [1.0, 2.0, 1.5, 1.0, 1.0]) # for the different classes, this performed best
+        self.weights = torch.tensor(weights)
+        print(f"Initialized {self.__class__.__name__} with weights: {weights}")
 
+    def __call__(self, pred_softmax, weak_target):
+        assert pred_softmax.shape == weak_target.shape
+        assert simplex(pred_softmax)
+        assert sset(weak_target, [0, 1])
+
+        weights = self.weights.to(pred_softmax.device)
+
+        log_p = (pred_softmax[:, self.idk, ...] + 1e-10).log()
+        mask = weak_target[:, self.idk, ...].float()
+        
+        weighted_mask = mask * weights[None, :, None, None]
+
+        loss = - einsum("bkwh,bkwh->", weighted_mask, log_p)
+        loss /= weighted_mask.sum() + 1e-10
+
+        return loss
+    
+class WeightedCeAndDiceCombinedLoss:
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', [0, 1, 2, 3, 4])
+        self.ce_weight = kwargs.get('ce_weight', 1.0)
+        self.dice_weight = kwargs.get('dice_weight', 1.0)
+        
+        weights = kwargs.get('weights', [1.0, 2.0, 1.5, 1.0, 1.0])
+        
+        self.ce_loss = WeightedCrossEntropy(idk=self.idk, weights=weights)
+        self.dice_loss = DiceLoss(idk=self.idk)
+        
+        print(f"Initialized {self.__class__.__name__} with ce_weight={self.ce_weight}, dice_weight={self.dice_weight}, class_weights={weights}")
+
+    def __call__(self, pred_softmax, weak_target):
+        ce = self.ce_loss(pred_softmax, weak_target)
+        dice = self.dice_loss(pred_softmax, weak_target)
+        
+        return self.ce_weight * ce + self.dice_weight * dice
 
 class PartialCrossEntropy(CrossEntropy):
     def __init__(self, **kwargs):
         super().__init__(idk=[1], **kwargs)
+
+class DiceLoss():
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', None)
+        self.smooth = kwargs.get('smooth', 1e-7)
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
+
+    def __call__(self, pred_softmax, weak_target):
+        assert pred_softmax.shape == weak_target.shape
+        assert simplex(pred_softmax)
+        assert sset(weak_target, [0, 1])
+
+        if self.idk is not None:
+            pred_softmax = pred_softmax[:, self.idk, ...]
+            weak_target = weak_target[:, self.idk, ...]
+
+        weak_target = weak_target.float()
+
+        intersection = einsum("bkwh,bkwh->bk", pred_softmax, weak_target)
+        pred_sum = einsum("bkwh->bk", pred_softmax)
+        target_sum = einsum("bkwh->bk", weak_target)
+
+        dice = (2 * intersection + self.smooth) / (pred_sum + target_sum + self.smooth)
+        
+        return 1 - dice.mean()
+
+class CeAndDiceCombinedLoss():
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', None)
+        self.ce_weight = kwargs.get('ce_weight', 1.0)
+        self.dice_weight = kwargs.get('dice_weight', 1.0)
+        
+        self.ce_loss = CrossEntropy(idk=self.idk)
+        self.dice_loss = DiceLoss(idk=self.idk)
+        
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
+
+    def __call__(self, pred_softmax, weak_target):
+        ce = self.ce_loss(pred_softmax, weak_target)
+        dice = self.dice_loss(pred_softmax, weak_target)
+        
+        return self.ce_weight * ce + self.dice_weight * dice
+
+class FocalTverskyLoss:
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', [0, 1, 2, 3, 4])
+        self.alpha = kwargs.get('alpha', 0.7)
+        self.beta = kwargs.get('beta', 0.3)
+        self.gamma = kwargs.get('gamma', 0.75)
+        self.smooth = kwargs.get('smooth', 1e-6)
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
+
+    def __call__(self, pred_softmax, target_one_hot):
+        assert pred_softmax.shape == target_one_hot.shape
+        assert simplex(pred_softmax)
+        assert sset(target_one_hot, [0, 1])
+        
+        if self.idk is not None:
+            pred_softmax = pred_softmax[:, self.idk, ...]
+            target_one_hot = target_one_hot[:, self.idk, ...]
+        
+        target_one_hot = target_one_hot.float()
+        
+        TP = einsum("bkwh,bkwh->bk", pred_softmax, target_one_hot)
+        FP = einsum("bkwh,bkwh->bk", pred_softmax, 1 - target_one_hot)
+        FN = einsum("bkwh,bkwh->bk", 1 - pred_softmax, target_one_hot)
+        
+        tversky = (TP + self.smooth) / (TP + self.alpha * FP + self.beta * FN + self.smooth)
+
+        focal_tversky = ((1 - tversky) ** self.gamma).mean()
+        
+        return focal_tversky
+    
+class TverskyLoss:
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', [0, 1, 2, 3, 4])
+        self.alpha = kwargs.get('alpha', 0.7)
+        self.beta = kwargs.get('beta', 0.3)
+        self.smooth = kwargs.get('smooth', 1.0)
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
+
+    def __call__(self, pred_softmax, target_one_hot):
+        assert pred_softmax.shape == target_one_hot.shape
+        assert simplex(pred_softmax)
+        assert sset(target_one_hot, [0, 1])
+        
+        if self.idk is not None:
+            pred_softmax = pred_softmax[:, self.idk, ...]
+            target_one_hot = target_one_hot[:, self.idk, ...]
+        
+        target_one_hot = target_one_hot.float()
+        
+        TP = einsum("bkwh,bkwh->bk", pred_softmax, target_one_hot)
+        FP = einsum("bkwh,bkwh->bk", pred_softmax, 1 - target_one_hot)
+        FN = einsum("bkwh,bkwh->bk", 1 - pred_softmax, target_one_hot)
+        
+        tversky = (TP + self.smooth) / (TP + self.alpha * FP + self.beta * FN + self.smooth)
+        
+        return 1.0 - tversky.mean()
+    
+class CeTverskyCombo:
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', [0, 1, 2, 3, 4])
+        self.ce_weight = kwargs.get('ce_weight', 0.5)
+        self.tversky_weight = kwargs.get('tversky_weight', 1.0)
+        
+        self.ce_loss = CrossEntropy(idk=self.idk)
+        self.tversky_loss = TverskyLoss(idk=self.idk, alpha=0.7, beta=0.3, smooth=1.0)
+        
+        print(f"Initialized {self.__class__.__name__} with {kwargs}")
+
+    def __call__(self, pred_softmax, weak_target):
+        ce = self.ce_loss(pred_softmax, weak_target)
+        tversky = self.tversky_loss(pred_softmax, weak_target)
+        
+        return self.ce_weight * ce + self.tversky_weight * tversky
+    
+class FocalDiceLoss:
+    def __init__(self, **kwargs):
+        self.idk = kwargs.get('idk', [0, 1, 2, 3, 4])
+        self.gamma = kwargs.get('gamma', 2.0)
+        self.alpha = kwargs.get('alpha', 0.25)
+        self.dice_weight = kwargs.get('dice_weight', 1.0)
+        self.focal_weight = kwargs.get('focal_weight', 1.0)
+        print(f"Initialized {self.__class__.__name__} with gamma={self.gamma}, focal_weight={self.focal_weight}, dice_weight={self.dice_weight}")
+
+    def __call__(self, pred_softmax, target):
+        pred = pred_softmax[:, self.idk, ...]
+        tgt = target[:, self.idk, ...].float()
+
+        ce = -(tgt * (pred + 1e-10).log())
+        focal_weight = (1 - pred) ** self.gamma
+        focal_loss = (self.alpha * focal_weight * ce).sum() / (tgt.sum() + 1e-10)
+        
+        intersection = einsum("bk...,bk...->bk", pred, tgt)
+        union = einsum("bk...->bk", pred) + einsum("bk...->bk", tgt)
+        dice = (2 * intersection + 1e-5) / (union + 1e-5)
+        dice_loss = 1 - dice.mean()
+        
+        return self.focal_weight * focal_loss + self.dice_weight * dice_loss
