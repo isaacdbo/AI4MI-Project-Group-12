6b6c8b59920162b7b6771d1684653258c11b46e5
Tom <t.de.haas2@student.vu.nl>
2025-10-13 10:36:14 +0200

    Added plotting script

---
+++ plot.sh
@@ -0,0 +1,4 @@
+python plot.py --metric_file results/SEGTHOR/ce/dice_tra.npy --dest results/SEGTHOR/ce/dice_tra.png --headless
+python plot.py --metric_file results/SEGTHOR/ce/dice_val.npy --dest results/SEGTHOR/ce/dice_val.png --headless
+python plot.py --metric_file results/SEGTHOR/ce/loss_tra.npy --dest results/SEGTHOR/ce/loss_tra.png --headless
+python plot.py --metric_file results/SEGTHOR/ce/loss_val.npy --dest results/SEGTHOR/ce/loss_val.png 
